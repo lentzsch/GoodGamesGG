dd35b073760ff4092d8a84bf2fd03df674092da2
James Lentzsch <jrlentzsch@gmail.com>
2023-02-20 10:03:31 -0800

    changed error to actual event

--- public/js/errorScript.js
+++ public/js/errorScript.js
@@ -2,10 +2,10 @@ window.addEventListener('DOMContentLoaded', async(event)=>{
     // const errorDiv = document.querySelector('.errors')
     // errorDiv.backgroundImage = `url("../../images/background-9.png")`
 
-    const message1 = `Welp, I don't know how we got lost. But now we're in space...`
+    const message1 = event
     const message2 = `Please use the navbar above to navigate back to a known page`
     const speed = 25;
-
+    console.log(event)
     for (let i=0;i<message1.length;i++){
         document.querySelector(`.errors__message1`).innerHTML+=message1[i]
         await new Promise(resolve => {
